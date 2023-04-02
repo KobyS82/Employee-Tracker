539c0d31f241d3892971eef9c3971dcd7b216b00
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-04-02 18:09:49 -0500

    added some employee schema code

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,15 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
+-- Path: db\schema.sql
+USE employee_db;
+
+-- Path: db\schema.sql
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
