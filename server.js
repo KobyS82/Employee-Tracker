fc63fbd4dbc5a17b64b842327cd8cf08394175ea
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-04-02 18:08:45 -0500

    fixed up server.js

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
-// const db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     // MySQL Username
-//     user: 'root',
-//     // TODO: Add MySQL Password
-//     password: '',
-//     database: 'books_db'
-//   },
-//   console.log(`Connected to the books_db database.`)
-// );
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL Username
+    user: 'root',
+    // MySQL Password
+    password: '',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the bemployee_db database.`)
+);
 
 //content
 app.get('/', (req, res) => {
